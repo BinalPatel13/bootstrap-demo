72b68bec1b5ed38f7a00e34a3d4c66f45311c58e
binal.patel <binal.patel.sa@gmail.com>
2020-05-22 10:11:31 +0530

    Initial static site template app

---
+++ index.js
@@ -0,0 +1,21 @@
+
+var typed = new Typed('#typed' , {
+    strings: [
+        'Junior Software Engineer',
+        'BE-IT Student'
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
+
+var typed_2 = new Typed('#typed_2' , {
+    strings: [
+        'Junior Software Engineer',
+        'BE-IT Student'
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
+
